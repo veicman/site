b54db4a6e7bf218792a8bc27ba9e455ae5249ca5
Alex <veicman@mail.ru>
2018-10-01 02:35:34 +1000

    new

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+        images = []
+        for filename in os.listdir('static/img'):
+                if filename.endswith(".jpg") and filename != "1.jpg":
+                        images.append(os.path.join('static/img', filename))
+                else:
+                        continue
+        return render_template('index.html',images=images)
 
 if __name__ == '__main__':
 	app.run()
