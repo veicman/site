223f6ce29afa11af89ee1c359a1dde507a30ef8c
Alex <veicman@mail.ru>
2018-10-02 14:19:12 +1000

    norm

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 import os
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+images = []
+for filename in os.listdir('static/img/big'):
+        if filename.endswith(".jpg"):
+                images.append(filename)
+        else:
+                continue
+
 @app.route('/')
 def index():
-        images = []
-        for filename in os.listdir('static/img/big'):
-                if filename.endswith(".jpg"):
-                        images.append(filename)
-                else:
-                        continue
-        return render_template('index.html',images=images,style="tiles")
-
+        return render_template('index.html',images=images,style="tiles",year=datetime.datetime.now().year)
 @app.route('/<style>')
 def style(style):
-        images = []
-        for filename in os.listdir('static/img/big'):
-                if filename.endswith(".jpg"):
-                        images.append(filename)
-                else:
-                        continue
-        return render_template('index.html',images=images,style=style)
+        return render_template('index.html',images=images,style=style,year=datetime.datetime.now().year)
 
 if __name__ == '__main__':
 	app.run()
